10f4d18c0fb5edc495d2ee68b6205601471e1c43
Artem Korolev <artemkorolevv@mail.ru>
2023-03-14 16:08:43 +0400

    init project

---
+++ static/js/index.js
@@ -0,0 +1,11 @@
+/* burger */
+
+const MENU_OPENED_CLASS = "burger-menu_opened";
+const burgerNode = document.querySelector(".burger-menu");
+const burgerControlNode = document.querySelector(".burger-menu__control");
+
+burgerControlNode.addEventListener('click' , (_event) => {
+    burgerNode.classList.toggle(MENU_OPENED_CLASS);
+
+});
+
